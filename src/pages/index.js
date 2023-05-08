e518577448f1d9d00e9095e69f94e1592d082521
Roren Natsumi <63963655+NatsumiXD@users.noreply.github.com>
2023-05-08 23:45:20 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/tutorial/intro">
+            to="/docs/tutorial/index">
             点此开始
           </Link>
         </div>
